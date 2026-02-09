7ced973f0fd1df9a17eb2ff987f007c097fa9360
lijay <670957859@qq.com>
2026-02-09 22:19:10 +0800

    新增流式和非流式对话模式，新增自定义拦截器，修改默认模型

---
+++ src/main/java/com/lijay/lijayaiagent/advisor/MyLoggerAdvisor.java
@@ -0,0 +1,57 @@
+package com.lijay.lijayaiagent.advisor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClientMessageAggregator;
+import org.springframework.ai.chat.client.ChatClientRequest;
+import org.springframework.ai.chat.client.ChatClientResponse;
+import org.springframework.ai.chat.client.advisor.api.CallAdvisor;
+import org.springframework.ai.chat.client.advisor.api.CallAdvisorChain;
+import org.springframework.ai.chat.client.advisor.api.StreamAdvisor;
+import org.springframework.ai.chat.client.advisor.api.StreamAdvisorChain;
+import org.springframework.ai.chat.messages.UserMessage;
+import reactor.core.publisher.Flux;
+
+/**
+ * 自定义日志Advisor
+ * 打印 info 级别日志， 只输出单次用户提示词和 Ai 回复的文本
+ */
+@Slf4j
+public class MyLoggerAdvisor implements CallAdvisor, StreamAdvisor {
+    
+    @Override
+    public ChatClientResponse adviseCall(ChatClientRequest chatClientRequest, CallAdvisorChain callAdvisorChain) {
+        this.logRequest(chatClientRequest);
+        ChatClientResponse chatClientResponse = callAdvisorChain.nextCall(chatClientRequest);
+        this.logResponse(chatClientResponse);
+        return chatClientResponse;
+    }
+    @Override
+    public Flux<ChatClientResponse> adviseStream(ChatClientRequest chatClientRequest, StreamAdvisorChain streamAdvisorChain) {
+        this.logRequest(chatClientRequest);
+        Flux<ChatClientResponse> chatClientResponses = streamAdvisorChain.nextStream(chatClientRequest);
+        return (new ChatClientMessageAggregator()).aggregateChatClientResponse(chatClientResponses, this::logResponse);
+    }
+
+    protected void logRequest(ChatClientRequest request) {
+        log.info("request: {}", ((UserMessage) request.prompt().getInstructions().get(1)).getText());
+    }
+    
+    protected void logResponse(ChatClientResponse chatClientResponse) {
+        if (chatClientResponse.chatResponse() != null) {
+            log.info("response: {}", chatClientResponse.chatResponse().getResults().getFirst().getOutput().getText());
+        }
+    }
+    @Override
+    public String getName() {
+        return this.getClass().getSimpleName();
+    }
+    @Override
+    public int getOrder() {
+        return 100;
+    }
+
+    public String toString() {
+        return MyLoggerAdvisor.class.getSimpleName();
+    }
+
+}
